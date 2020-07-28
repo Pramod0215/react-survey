198692fa75de15b07a73cd0818608a92a4a7f2aa
Pramod0215 <you@example.com>
2020-07-28 15:10:35 +0530

    react-survey

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import "./App.css";
+import { HomePage } from "./Home";
+import { CreatorPage } from "./Creator";
+import { SurveyPage } from "./Survey";
+import { ExportToPDFPage } from "./Export";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function SurveyJSReactApplication() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="/">SurveyJS + ReactJS</a>
+            </div>
+            <ul className="nav navbar-nav">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/survey">Survey</Link>
+              </li>
+              <li>
+                <Link to="/creator">SurveyJS Creator</Link>
+              </li>
+              <li>
+                <Link to="/export">Export to PDF</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/survey">
+            <SurveyPage />
+          </Route>
+          <Route path="/creator">
+            <CreatorPage />
+          </Route>
+          <Route path="/export">
+            <ExportToPDFPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
